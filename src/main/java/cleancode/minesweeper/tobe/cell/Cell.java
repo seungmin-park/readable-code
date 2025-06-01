0ced563b9e89540c78303f66bec7df952c593a84
seungmin <tmddudals369@naver.com>
2025-06-02 02:44:17 +0900

    refactor: 상속 관계 조합으로 변경

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,35 +1,21 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
+public interface Cell {
     //열림 닫힘에서 체크 여부로 도메인 지식 변경
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_CELL_SIGN = "□";
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_CELL_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
+    boolean isLandMine();
 
-    // Cell의 속성: 근처 지뢰 숫자, 지뢰 여부
-    // Cell의 상태: 깃발 유무, 열림, 닫힘, 사용자가 확인함
+    boolean hasLandMineCount();
 
-    public abstract boolean isLandMine();
+    String getSign();
 
-    public abstract String getSign();
+    void flag();
 
-    public void flag() {
-        this.isFlagged = true;
-    }
+    void open();
 
-    public void open() {
-        this.isOpened = true;
-    }
+    boolean isChecked();
 
-    public boolean isChecked() {
-        return isFlagged || isOpened;
-    }
-
-    public boolean isOpened() {
-        return isOpened;
-    }
-
-    public abstract boolean hasLandMineCount();
+    boolean isOpened();
 }
